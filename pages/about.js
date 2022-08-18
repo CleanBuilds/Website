0440b8064af59d95085b4638832707631c2c0e75
hhuumm <h4mid@hotmail.com>
2022-08-18 16:13:16 -0400

    Adding logo and new pages

--- pages/about.js
+++ pages/about.js
@@ -2,25 +2,25 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Logo from '../public/PhotoShopped.png'
 
-const Welcome = () => {
+const About = () => {
   return (
     <div className="about-wrapper">
       <div className="about">
-      <h2 className="logo"> <img
+        <h2 className="logo"> <img
                   src={Logo.src}
                   className="cart-product-image"
                
                 /></h2>
         <p className="description">
-        This is a startup company with goals of bringing high quality desktop
+          This is a startup company with goals of bringing high quality desktop
           computers to market. Our goal is to not cut corners, allowing our
           computers to be both clean in design and performance.The goal is to be
           one of the first to market with the new generation of desktop
-          computers said to release end of Q4. So when everybody is getting that new computer, you'll know where to come.
+          computers said to release end of Q4
         </p>
-        <Link href="/store">
+        <Link href="/">
           <button type="button" width="300px" className="btn">
-            Begin Shopping
+            Continue Shopping
           </button>
         </Link>
       </div>
@@ -28,4 +28,4 @@ const Welcome = () => {
   );
 };
 
-export default Welcome;
+export default About;
